2bf552d0abb13551e87c4051ddf73464d80bc24c
Alex <aibogomoloff@yandex.ru>
2024-10-08 20:45:24 +0300

    Init commit

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,7 +7,7 @@ private:
 
 public:
     Calculator() : num1(0), num2(0) {}
-   
+
     void set(double n1, double n2) {
         num1 = n1;
         num2 = n2;
@@ -30,66 +30,40 @@ public:
     }
 
     double divide_1_2() {
-       // if (num2 != 0) {
-            return num1 / num2;
-        }/*
-        else {
-            std::cerr << "Ошибка: деление на ноль" << std::endl;
-            return 0; // Возвращаем 0, но это можно изменить в зависимости от требований
-        }
-    }*/
-
-    double divide_2_1() {
-       // if (num1 != 0) {
-            return num2 / num1;
-        }/*
-        else {
-            std::cerr << "Ошибка: деление на ноль" << std::endl;
-            return 0; // Возвращаем 0, но это можно изменить в зависимости от требований
-        }
-    }*/
-
-    /*bool set_num1(double value) {
-        if (value != 0) {
-            num1 = value;
-            return true;
-        }
-        return false;
+        
+        return num1 / num2;
     }
 
-    bool set_num2(double value) {
-        if (value != 0) {
-            num2 = value;
-            return true;
-        }
-        return false;
-    }*/
+    double divide_2_1() {
+     
+        return num2 / num1;
+    }
 };
 
 int main() {
 
     while (true)
     {
-    Calculator calculator;
+        Calculator calculator;
 
-    setlocale(LC_ALL, "RUS");
-    double input1, input2;
-    std::cout << "Введите num1: ";
-    std::cin >> input1;
-    if (input1 == 0) {
-        std::cout << "Неверный ввод!" << std::endl;
-        continue;
-    }
-    std::cout << "Введите num2: ";
-    std::cin >> input2;
-    if (input2 == 0) {
-        std::cout << "Неверный ввод!" << std::endl;
-        continue;
-    }
+        setlocale(LC_ALL, "RUS");
+        double input1, input2;
+        std::cout << "Введите num1: ";
+        std::cin >> input1;
+        if (input1 == 0) {
+            std::cout << "Неверный ввод!" << std::endl;
+            continue;
+        }
+        std::cout << "Введите num2: ";
+        std::cin >> input2;
+        if (input2 == 0) {
+            std::cout << "Неверный ввод!" << std::endl;
+            continue;
+        }
 
 
 
-    calculator.set(input1, input2);
+        calculator.set(input1, input2);
 
 
         std::cout << "num1 + num2 = " << calculator.add() << std::endl;
